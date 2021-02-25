81e60003da650a726913a552fa9c50f5bf7ce75e
Leo Chen <78456101+LeoChen21@users.noreply.github.com>
2021-02-25 09:47:38 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+from display import *
+from draw import *
+
+s = new_screen()
+c = [ 0, 255, 0 ]
+
+#octants 1 and 5
+#bottom left to top right
+draw_line(0, 0, XRES-1, YRES-1, s, c)
+#bottom left to mid right
+draw_line(0, 0, XRES-1, YRES / 2, s, c)
+#top right to mid left
+draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
+
+#octants 8 and 4
+c[BLUE] = 255;
+#top left to bottom right
+draw_line(0, YRES-1, XRES-1, 0, s, c);
+#top left to middle right
+draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
+#bottom right to middle left
+draw_line(XRES-1, 0, 0, YRES/2, s, c);
+
+#octants 2 and 6
+c[RED] = 255;
+c[GREEN] = 0;
+c[BLUE] = 0;
+#bottom left to top middle
+draw_line(0, 0, XRES/2, YRES-1, s, c);
+#top right to bottom middle
+draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
+
+#octants 7 and 3
+c[BLUE] = 255;
+#top left to bottom middle
+draw_line(0, YRES-1, XRES/2, 0, s, c);
+#bottom right to top middle
+draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
+
+#horizontal and vertical
+c[BLUE] = 0;
+c[GREEN] = 255;
+#horizontal line
+draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
+#vertical line
+draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
+
+
+display(s)
+save_ppm(s, 'binary.ppm')
+save_ppm_ascii(s, 'ascii.ppm')
+save_extension(s, 'img.png')
